c07cd4384d4769a24e9b8763e28a93035ed6ad0d
mary913 <Patrivimwenge@gmail.com>
2024-09-18 22:22:23 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat (name) {
+    cats.push("Ralph")
+}
+function destructivelyPrependCat(name){
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function appendCat(name){
+  const newCats = cats.slice();
+  newCats.push("Broom");
+  return newCats;
+}
+
+function prependCat(name) {
+    const newCats = cats.slice();
+    newCats.unshift("Arnold");
+    return newCats;
+}
+function removeLastCat(){
+    newCats = cats.slice();
+    newCats.pop();
+    return newCats;
+}
+function removeFirstCat(){
+    const newCats = cats.slice();
+    newCats.shift();
+    return newCats;
+}
+function removeLastCat() {
+    let newCats = cats.slice(0, -1); // Create a new array excluding the last element
+    return newCats;
+}
